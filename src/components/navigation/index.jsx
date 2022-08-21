969af6d1b1ee206b0bc22f16a926497ade2e0ddc
mp40 <philps.matthew@gmail.com>
2022-08-21 11:23:16 +0200

    use css for mobile view

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import MediaQueryLarge from "../media-query/media-query-large";
 import { Link } from "gatsby";
-import { container } from "./navigation.module.css";
+import { container, smallContainer } from "./navigation.module.css";
 
 const Navigation = () => {
   return (
-    <MediaQueryLarge>
+    <>
       <div className={container}>
         <Link to="/">Home</Link>
         <Link to="/hand-to-hand">Hand To Hand</Link>
         <Link to="/shotguns">Shotguns</Link>
       </div>
-    </MediaQueryLarge>
+      <div className={smallContainer}>
+        <Link to="/">Home</Link>
+      </div>
+    </>
   );
 };
 
