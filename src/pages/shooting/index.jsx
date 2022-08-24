870898fb8251c4d5f077bd476cc8ec8bf98be713
mp40 <philps.matthew@gmail.com>
2022-08-24 19:21:38 +0200

    improve shooting page

--- src/pages/shooting/index.jsx
+++ src/pages/shooting/index.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../../components/layout";
 import Shooting from "../../components/shooting";
+import HitChance from "../../components/hit-chance";
+
+import { container, left } from "./shooting.module.css";
 
 export const Component = () => {
+  const [length, setLength] = useState(1);
+
   return (
     <div>
       <h1>Shooting</h1>
-      <Shooting />
+      <div className={container}>
+        <div className={left}>
+          <div>
+            <button type="button" onClick={() => setLength(length + 1)}>
+              add
+            </button>
+            {length > 1 && (
+              <button type="button" onClick={() => setLength(length - 1)}>
+                remove last
+              </button>
+            )}
+          </div>
+          {[...Array(length).keys()].map((i) => {
+            return <Shooting key={i} />;
+          })}
+        </div>
+        <HitChance />
+      </div>
     </div>
   );
 };
